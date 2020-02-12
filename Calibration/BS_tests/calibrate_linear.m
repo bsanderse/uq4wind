8e674978c24f56d3a325812c1ecfba8800744f28
Benjamin Sanderse <30893600+bsanderse@users.noreply.github.com>
2020-02-12 14:57:25 +0100

    updates to linear calibration case
    * improvements to IQR code (more options can be assessed via function call, e.g. the option to use the C++ code)
    * changed likelihood definition (bug?)
    * return number of model evaluations
    * perform posterior predictive calculations with the IQR

--- Calibration/BS_tests/calibrate_linear.m
+++ Calibration/BS_tests/calibrate_linear.m
@@ -1,4 +1,4 @@
-function [x, w, err, n] = calibrate_linear(u, varargin)
+function [x, w, err, n, model_evals] = calibrate_linear(u, varargin)
 % [x, w, err, n] = CALIBRATE(u, ...) - Construct calibration case and pass
 % it to analyticnd.
 %
@@ -24,7 +24,7 @@ function [x, w, err, n] = calibrate_linear(u, varargin)
 % prior - If true, does not uses the posterior, but the prior for the
 %         calibration. Can be used for comparison.
 %         Default: false
-%
+% 
 % truemean/I are determined by this function and cannot be overwritten.
 %
 % Output of the function is exactly the output obtained by running
@@ -34,10 +34,10 @@ function [x, w, err, n] = calibrate_linear(u, varargin)
 % See also ANALYTICND
 
 %% Parse optional function arguments
-nz = 20;
+% nz = 20;
 sigma = 1/10;
 d = 5;
-x0 = [];
+% x0 = [];
 S = 1e5; % Used for estimation of truemean
 use_prior = false;
 varargin_analyticnd = {};
@@ -61,32 +61,43 @@ for i=1:2:length(varargin)
             A = varargin{i+1};
         case {'truemean', 'I'} % Determined in this function.
             warning('Option %s ignored.', varargin{i});
+        case 'z'
+           z = varargin{i+1}; 
         otherwise
             varargin_analyticnd{end+1} = varargin{i};
             varargin_analyticnd{end+1} = varargin{i+1};
     end
 end
 
-if isempty(x0)
-    x0 = ones(1, d)/2;
-end
-
-if length(x0) ~= d
-    warning('Size of x0 does not match d. Resetting to default.');
-    x0 = ones(1, d)/2;
-end
+% if isempty(x0)
+%     x0 = ones(1, d)/2;
+% end
+% 
+% if length(x0) ~= d
+%     warning('Size of x0 does not match d. Resetting to default.');
+%     x0 = ones(1, d)/2;
+% end
 
 %% Construct statistical model, so data, prior, likelihood, posterior
 I = [0, 1];
-z = u(x0,A) + normrnd(0, sigma, 1, nz);
+% z = u(x0,A) + normrnd(0, sigma, 1, nz);
 prior = @(x) ones(size(x, 1), 1);
-likelihood = @(x) exp(- sum((u(x,A)-z).^2, 2) ./ sigma );
+% 1/ sqrt( (2*pi)^d * det(Sigma) ) * exp ( -(1/2) * (u-z)' * Sigma^{-1} *
+% (u-z) )
+% for the simple case Sigma = sigma^2*I, we have
+% det(Sigma) = (sigma^2)^d
+% Sigma^{-1} = 1/sigma^2 * I
+% this gives the multiplicative factor 1/ sqrt( (2*pi)^d * (sigma^2)^d ); 
+% however, this is just a scaling that is not important for the analysis
+likelihood = @(x) exp(- sum((u(x,A)-z).^2, 2) ./ (2*sigma^2) ); 
 posterior = @(x) likelihood(x).*prior(x);
 
 %% Construct (approximation) of true mean using MC
-X = rand(S, d);
-evidence = mean(posterior(X));
-truemean = mean(X.*posterior(X)) / evidence;
+% X = rand(S, d); % this assumes uniform distributed RV
+X = rand(1e5,d);
+post     = posterior(X);
+evidence = mean(post); % note, this involves S model evaluations
+truemean = mean(X.*post) / evidence;
 clear X
 
 %% Finally, overwrite sampler if user wants that
@@ -99,7 +110,7 @@ if use_prior
 end
 
 %% Run analyticnd with the obtained posterior and truemean estimate
-[x, w, err, n] = analyticnd(d, posterior, ...
+[x, w, err, n, model_evals] = analyticnd(d, posterior, ...
     'truemean', truemean, ...
     'I', I, ...
     varargin_analyticnd{:});
--- Calibration/BS_tests/linearmodel.m
+++ Calibration/BS_tests/linearmodel.m
@@ -1,9 +1,14 @@
 function Y = linearmodel(B,X)
-% B: uncertain parameter, size p*1
+% B: uncertain parameter, size p*1, or p*M
 % X: contains independent variable, size N*p: 
+% p: dimension of parameter vector
+% N: number of locations
+% M: number of parameter vectors to be evaluated
+
+% output Y: size N*M 
 
 % y = b_1 + b_2*x 
-Y = B(1) + X*B(2);
+Y = B(1,:) + cos(X)*B(2,:);
 
 % y = b_1 + b_2*x + b_3*x^2
 % Y = B(1) + X*B(2) + (X.^2)*B(3);
