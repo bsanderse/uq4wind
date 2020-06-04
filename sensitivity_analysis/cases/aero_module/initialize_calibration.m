f0af6b123ae380d9a1a1c47d95b8068cb25ea36a
VinitDighe <VinitDighe@users.noreply.github.com>
2020-06-04 10:30:05 +0200

    ...

--- sensitivity_analysis/cases/aero_module/initialize_calibration.m
+++ sensitivity_analysis/cases/aero_module/initialize_calibration.m
@@ -13,9 +13,9 @@ Model.isVectorized = false;
 
 %% Experimental data
 % Marco's script reading data in N/m
-filename_exp = ('..\..\..\Experimental/WINDTRUE\raw.dat');
+filename_exp = ('../../../Experimental/WINDTRUE/raw.dat');
 output_raw = importfile3(filename_exp, 2);
-Data.y = [output_raw.Fy03, output_raw.Fy05, output_raw.Fy08,
+Data.y = [output_raw.Fy03, output_raw.Fy05, output_raw.Fy08,...
           output_raw.Fy10]; % Raw data
 % Data.y = [mean(output_raw.Fy03), mean(output_raw.Fy05), mean(output_raw.Fy08),...
 %           mean(output_raw.Fy10)]; % Mean data
