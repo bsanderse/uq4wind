e79e59f14d65ed0742b978f78384eb02bee69119
VinitDighe <VinitDighe@users.noreply.github.com>
2020-09-08 15:50:13 +0200

    Final changes for the workshop

--- sensitivity_analysis/cases/aero_module/write_calibration.m
+++ sensitivity_analysis/cases/aero_module/write_calibration.m
@@ -4,21 +4,23 @@
 % Note: The calibrated files for including the headerlines (necessary for Aero-Module run)
 % is written in cal_files.m
 
-sec03 = readtable(['C:\Users\kriek\Desktop\windtrue\sensitivity_analysis\AEROmodule\NM80_calibrate/section03_ref.dat']);
+addpath('../../AEROmodule/NM80_calibrate')
+
+sec03 = readtable('section03_ref.dat');
 p1 = table(sec03.Var1, sec03.Var2*(1+Delta_1), sec03.Var3, sec03.Var4);
-writetable(p1, 'C:\Users\kriek\Desktop\windtrue\sensitivity_analysis\AEROmodule\NM80_calibrate/3c.dat', 'Delimiter',' ', 'WriteVariableNames',0);
+writetable(p1, '3c.dat', 'Delimiter',' ', 'WriteVariableNames',0);
 
-sec05 = readtable(['C:\Users\kriek\Desktop\windtrue\sensitivity_analysis\AEROmodule\NM80_calibrate/section05_ref.dat']);
+sec05 = readtable('section05_ref.dat');
 p2 = table(sec05.Var1, sec05.Var2*(1+Delta_2), sec05.Var3, sec05.Var4);
-writetable(p2, 'C:\Users\kriek\Desktop\windtrue\sensitivity_analysis\AEROmodule\NM80_calibrate/5c.dat', 'Delimiter',' ', 'WriteVariableNames',0);
+writetable(p2, '5c.dat', 'Delimiter',' ', 'WriteVariableNames',0);
 
-sec08 = readtable(['C:\Users\kriek\Desktop\windtrue\sensitivity_analysis\AEROmodule\NM80_calibrate/section08_ref.dat']);
+sec08 = readtable('section08_ref.dat');
 p3 = table(sec08.Var1, sec08.Var2*(1+Delta_3), sec08.Var3, sec08.Var4);
-writetable(p3, 'C:\Users\kriek\Desktop\windtrue\sensitivity_analysis\AEROmodule\NM80_calibrate/8c.dat', 'Delimiter',' ', 'WriteVariableNames',0);
+writetable(p3, '8c.dat', 'Delimiter',' ', 'WriteVariableNames',0);
 
-sec10 = readtable(['C:\Users\kriek\Desktop\windtrue\sensitivity_analysis\AEROmodule\NM80_calibrate/section10_ref.dat']);
+sec10 = readtable('section10_ref.dat');
 p4 = table(sec10.Var1, sec10.Var2*(1+Delta_4), sec10.Var3, sec10.Var4);
-writetable(p4, 'C:\Users\kriek\Desktop\windtrue\sensitivity_analysis\AEROmodule\NM80_calibrate/10c.dat', 'Delimiter',' ', 'WriteVariableNames',0);
+writetable(p4, '10c.dat', 'Delimiter',' ', 'WriteVariableNames',0);
 
 %% Plotting
 
