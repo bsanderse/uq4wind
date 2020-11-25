e72b39f15aec532b4c1cf716b26a3d9c643e84b6
GVerikios <GVerikios@users.noreply.github.com>
2020-11-26 00:42:27 +0100

    Update NewMexico.m
    Defined new values and bounds for the 3D parameters.

--- sensitivity_analysis/cases/NewMexico_sensitivity/NewMexico.m
+++ sensitivity_analysis/cases/NewMexico_sensitivity/NewMexico.m
@@ -4,20 +4,20 @@ function Input  = NewMexico()
 counter = 0;
 %% define marginals of Snel 3D Correction model in case of performing 3D correction outside of Aeromodule
 counter = counter+1;
-factor3D     = 3.1;
+factor3D     = 2.2;
 factor3D_Std = 0.2;  % Standard deviation
-factor3D_LB = 1; % Lower bound of truncated Gaussian distribution
-factor3D_UB = 5;  % Upper bound of truncated Gaussian distribution 
+factor3D_LB = 1.5; % Lower bound of truncated Gaussian distribution
+factor3D_UB = 3.5;  % Upper bound of truncated Gaussian distribution 
 Input.Marginals(counter).Name = 'factor3D';
 Input.Marginals(counter).Type = 'Gaussian'; 
 Input.Marginals(counter).Parameters = [factor3D , factor3D_Std*abs(factor3D)];
 Input.Marginals(counter).Bounds = [factor3D_LB factor3D_UB]; 
 
 counter = counter+1;
-exp3D     = 2;
+exp3D     = 4;
 exp3D_Std = 0.2;  % Standard deviation
-exp3D_LB = 1; % Lower bound of truncated Gaussian distribution
-exp3D_UB = 3;  % Upper bound of truncated Gaussian distribution 
+exp3D_LB = 2; % Lower bound of truncated Gaussian distribution
+exp3D_UB = 5;  % Upper bound of truncated Gaussian distribution 
 Input.Marginals(counter).Name = 'exp3D';
 Input.Marginals(counter).Type = 'Gaussian'; 
 Input.Marginals(counter).Parameters = [exp3D, exp3D_Std*abs(exp3D)];
