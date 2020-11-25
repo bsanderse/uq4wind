4fa4add2c7648e0c175020e83a175710d0d28731
GVerikios <GVerikios@users.noreply.github.com>
2020-11-25 18:30:53 +0100

    Update write_polars.m
    Fixed a bug with a filename.

--- sensitivity_analysis/AEROmoduleWrapper/3D_correction/write_polars.m
+++ sensitivity_analysis/AEROmoduleWrapper/3D_correction/write_polars.m
@@ -132,7 +132,7 @@ addpath(fullfile(root_folder,'AEROmoduleWrapper\3D_correction\2D_polars'));
             fprintf(fileID,'\n');
             fprintf(fileID,'Reynolds_Nr %f\n',Re(i));
             
-            writematrix(polar_mat, section{i}, 'WriteMode', 'Append', 'Delimiter', '\t');
+            writematrix(polar_mat, filevar, 'WriteMode', 'Append', 'Delimiter', '\t');
             
             disp([section{i} ' polar file created!'])
             
