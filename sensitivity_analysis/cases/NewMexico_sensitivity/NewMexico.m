104746c242d55f4a330aa5a304620e4dd2b29af8
GVerikios <GVerikios@users.noreply.github.com>
2020-12-13 15:02:18 +0100

    Update NewMexico.m
    Changed mean to correspond to Snel model

--- sensitivity_analysis/cases/NewMexico_sensitivity/NewMexico.m
+++ sensitivity_analysis/cases/NewMexico_sensitivity/NewMexico.m
@@ -7,7 +7,7 @@ counter = 0;
 %% define marginals of Snel 3D Correction model in case of performing 3D correction outside of Aeromodule
 
         counter = counter+1;
-        factor3D     = 2.2;
+        factor3D     = 3;
         factor3D_Std = 0.1;  % Standard deviation
         % factor3D_LB = 1.5; % Lower bound of truncated Gaussian distribution
         % factor3D_UB = 3.5;  % Upper bound of truncated Gaussian distribution 
@@ -17,7 +17,7 @@ counter = 0;
         % Input.Marginals(counter).Bounds = [factor3D_LB factor3D_UB]; 
 
         counter = counter+1;
-        exp3D     = 4;
+        exp3D     = 2;
         exp3D_Std = 0.1;  % Standard deviation
         % exp3D_LB = 2; % Lower bound of truncated Gaussian distribution
         % exp3D_UB = 5;  % Upper bound of truncated Gaussian distribution 
