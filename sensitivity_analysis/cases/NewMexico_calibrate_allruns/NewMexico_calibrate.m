869ee5756009b0a5a0254dccba693fbc0344d340
GVerikios <GVerikios@users.noreply.github.com>
2021-01-24 04:34:07 +0100

    Update NewMexico_calibrate.m
    Set up the model for 3D correction

--- sensitivity_analysis/cases/NewMexico_calibrate_allruns/NewMexico_calibrate.m
+++ sensitivity_analysis/cases/NewMexico_calibrate_allruns/NewMexico_calibrate.m
@@ -1,92 +1,72 @@
 function Input  = NewMexico_calibrate()
 
-% initialize counter of marginals
+%FixedParameters = P.FixedParameters;
+
+% type = FixedParameters.correction;
+
+%% Select Correction type, type = 1 -->Snel, type = 2 --> Chaviaropoulos - Hansen
+
+type = 1 ; 
+
 counter = 0;
+    switch type
+    
+    % initialize counter of marginals
+    
 
-% define marginals of yaw model
-counter = counter+1;
-AM11     = 0.445;
-AM11_Std = 0.2;  % Standard deviation
-% AM11_LB = 0; % Lower bound of truncated Gaussian distribution
-% AM11_UB = 1;  % Upper bound of truncated Gaussian distribution 
-Input.Marginals(counter).Name = 'AM11';
-Input.Marginals(counter).Type = 'Gaussian'; 
-Input.Marginals(counter).Parameters = [AM11, AM11_Std*abs(AM11)];
-% Input.Marginals(counter).Bounds = [AM11_LB AM11_UB]; 
+    %% If correction = 1 --> Snel
+    case {1}
+    
+        counter = counter+1;
+        factor3D     = 3;
+        factor3D_Std = 0.1;  % Standard deviation
+        % factor3D_LB = 1.5; % Lower bound of truncated Gaussian distribution
+        % factor3D_UB = 3.5;  % Upper bound of truncated Gaussian distribution 
+        Input.Marginals(counter).Name = 'factor3D';
+        Input.Marginals(counter).Type = 'Gaussian'; 
+        Input.Marginals(counter).Parameters = [factor3D , factor3D_Std*abs(factor3D)];
+        % Input.Marginals(counter).Bounds = [factor3D_LB factor3D_UB]; 
 
-counter = counter+1;
-AM12     = -1.78;
-AM12_Std = 0.2;  % Standard deviation
-% AM12_LB = 0; % Lower bound of truncated Gaussian distribution
-% AM12_UB = 1;  % Upper bound of truncated Gaussian distribution 
-Input.Marginals(counter).Name = 'AM12';
-Input.Marginals(counter).Type = 'Gaussian'; 
-Input.Marginals(counter).Parameters = [AM12, AM12_Std*abs(AM12)];
-% Input.Marginals(counter).Bounds = [AM12_LB AM12_UB]; 
+        counter = counter+1;
+        exp3D     = 2;
+        exp3D_Std = 0.1;  % Standard deviation
+        % exp3D_LB = 2; % Lower bound of truncated Gaussian distribution
+        % exp3D_UB = 5;  % Upper bound of truncated Gaussian distribution 
+        Input.Marginals(counter).Name = 'exp3D';
+        Input.Marginals(counter).Type = 'Gaussian'; 
+        Input.Marginals(counter).Parameters = [exp3D, exp3D_Std*abs(exp3D)];
+        % Input.Marginals(counter).Bounds = [exp3D_LB exp3D_UB]; 
 
-counter = counter+1;
-AM13     = 1.63;
-AM13_Std = 0.2;  % Standard deviation
-% AM13_LB = 0; % Lower bound of truncated Gaussian distribution
-% AM13_UB = 1;  % Upper bound of truncated Gaussian distribution 
-Input.Marginals(counter).Name = 'AM13';
-Input.Marginals(counter).Type = 'Gaussian'; 
-Input.Marginals(counter).Parameters = [AM13, AM13_Std*abs(AM13)];
+        %% If correction = 2 --> Chaviaropoulos - Hansen
+    case {2}
+        
+        %% Chav. - Hansen model
+        counter = counter+1;
+        factor3D     = 2.2;
+        factor3D_Std = 0.1;  % Standard deviation
+        % factor3D_LB = 1.5; % Lower bound of truncated Gaussian distribution
+        % factor3D_UB = 3.5;  % Upper bound of truncated Gaussian distribution 
+        Input.Marginals(counter).Name = 'factor3D';
+        Input.Marginals(counter).Type = 'Gaussian'; 
+        Input.Marginals(counter).Parameters = [factor3D , factor3D_Std*abs(factor3D)];
+        % Input.Marginals(counter).Bounds = [factor3D_LB factor3D_UB]; 
 
-counter = counter+1;
-AM14     = -0.0543;
-AM14_Std = 0.2;  % Standard deviation
-% AM14_LB = 0; % Lower bound of truncated Gaussian distribution
-% AM14_UB = 1;  % Upper bound of truncated Gaussian distribution 
-Input.Marginals(counter).Name = 'AM14';
-Input.Marginals(counter).Type = 'Gaussian'; 
-Input.Marginals(counter).Parameters = [AM14, AM14_Std*abs(AM14)];
+        counter = counter+1;
+        exp3D     = 4;
+        exp3D_Std = 0.1;  % Standard deviation
+        % exp3D_LB = 2; % Lower bound of truncated Gaussian distribution
+        % exp3D_UB = 5;  % Upper bound of truncated Gaussian distribution 
+        Input.Marginals(counter).Name = 'exp3D';
+        Input.Marginals(counter).Type = 'Gaussian'; 
+        Input.Marginals(counter).Parameters = [exp3D, exp3D_Std*abs(exp3D)];
+        % Input.Marginals(counter).Bounds = [exp3D_LB exp3D_UB];
+    otherwise
+        
+        error('Invalid correction type')
+        
+    end
 
-counter = counter+1;
-AM15     = 0.367;
-AM15_Std = 0.2;  % Standard deviation
-% AM14_LB = 0; % Lower bound of truncated Gaussian distribution
-% AM14_UB = 1;  % Upper bound of truncated Gaussian distribution 
-Input.Marginals(counter).Name = 'AM15';
-Input.Marginals(counter).Type = 'Gaussian'; 
-Input.Marginals(counter).Parameters = [AM15, AM15_Std*abs(AM15)];
-
-% counter = counter+1;
-% Input.Marginals(counter).Name = 'CL';
-% Input.Marginals(counter).Airfoil = 'risoA121.dat';
-% Input.Marginals(counter).AlphaPert = [-10 30];
-% Input.Marginals(counter).AirfoilIndex = 1; % unique ID corresponding to airfoil
-% Input.Marginals(counter).Type = 'Uniform'; 
-% Input.Marginals(counter).Parameters = [-1.5 1.5];
-% Input.Marginals(counter).Bounds = [-1.5 1.5];
-
-% counter = counter+1;
-% Input.Marginals(counter).Name = 'CL';
-% Input.Marginals(counter).Airfoil = 'du91w250.dat';
-% Input.Marginals(counter).AlphaPert = [-10 30];
-% Input.Marginals(counter).AirfoilIndex = 3; % unique ID corresponding to airfoil
-% Input.Marginals(counter).Type = 'Uniform'; 
-% Input.Marginals(counter).Parameters = [-1.5 1.5];
-% Input.Marginals(counter).Bounds = [-1.5 1.5];
-
-% counter = counter+1;
-% Input.Marginals(counter).Name = 'CL';
-% Input.Marginals(counter).Airfoil = 'naca64418_clean_Re0.7M.dat';
-% Input.Marginals(counter).AlphaPert = [-10 30];
-% Input.Marginals(counter).AirfoilIndex = 4; % unique ID corresponding to airfoil
-% Input.Marginals(counter).Type = 'Uniform'; 
-% Input.Marginals(counter).Parameters = [-1.5 1.5];
-% Input.Marginals(counter).Bounds = [-1.5 1.5];% 
-
-% counter = counter+1;
-% Input.Marginals(counter).Name = 'CD';
-% Input.Marginals(counter).Airfoil = 'naca64418_clean_Re0.7M.dat';
-% Input.Marginals(counter).AlphaPert = [0 20];
-% Input.Marginals(counter).AirfoilIndex = 4; % unique ID corresponding to airfoil
-% Input.Marginals(counter).Type = 'Uniform'; 
-% Input.Marginals(counter).Parameters = [-1.5 1.5];
-% Input.Marginals(counter).Bounds = [-1.5 1.5];
-
+end
 
 
 end
