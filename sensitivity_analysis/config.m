63a41d4da1cb5db9319fdfbba962056fdb404f2e
GVerikios <GVerikios@users.noreply.github.com>
2021-02-01 14:28:37 +0100

    Update config.m
    Run with previous version

--- sensitivity_analysis/config.m
+++ sensitivity_analysis/config.m
@@ -1,4 +1,4 @@
 %UQLab_path = 'C:\Users\pkumar\Dropbox\WindTrue\UQLab\';
-UQLab_path = 'C:\Users\kriek\Desktop\windtrue\sensitivity_analysis\UQLabCore_Rel1.3.0';
+%UQLab_path = 'C:\Users\kriek\Desktop\windtrue\sensitivity_analysis\UQLabCore_Rel1.3.0';
 % UQLab_path = '/Users/sanderse/Dropbox/work/Programming/UQ/UQLabCore_Rel1.3.0/';
-%UQLab_path = '\\mac\home\Dropbox\work\Programming\UQ\windtrue\sensitivity_analysis';
+UQLab_path = 'C:\Users\gveri\Documents\GitHub\windtrue\sensitivity_analysis\UQLabCore_Rel1.3.0';
