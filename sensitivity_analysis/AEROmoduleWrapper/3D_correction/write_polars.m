63fa053cf3e5d854220bf302f5908d0b698bbfbc
GVerikios <GVerikios@users.noreply.github.com>
2021-02-08 14:20:03 +0100

    Update write_polars.m
    need to specify the folder to write the polars, for future improvement take that folder from input in turbineName

--- sensitivity_analysis/AEROmoduleWrapper/3D_correction/write_polars.m
+++ sensitivity_analysis/AEROmoduleWrapper/3D_correction/write_polars.m
@@ -55,10 +55,10 @@
         for i = 1 : size(files1,2)
 
             [polar_mat, Re(i)] = snel_3Dcor(k,x,files1{i},c(i),r(i));
-            
+           
             %%%%%check if folder changes
             
-            filevar = fullfile(wr_path,'AEROmodule\NewMexico\current',section{i}); % path to polar in current folder for Aeromodule
+            filevar = fullfile(wr_path,'AEROmodule\NewMexico_calibrate\current',section{i}); % path to polar in current folder for Aeromodule
             
             fileID = fopen(filevar,'w');
 
@@ -127,7 +127,7 @@
             [polar_mat, Re(i)] = chav_hansen_3D(k,x,files1{i},c(i),r(i),pitch,twist(i));
 
             %%%%%check if folder changes
-            filevar = fullfile(wr_path,'AEROmodule\NewMexico\current',section{i}); % path to polar in current folder for Aeromodule
+            filevar = fullfile(wr_path,'AEROmodule\NewMexico_calibrate\current',section{i}); % path to polar in current folder for Aeromodule
             
             fileID = fopen(filevar,'w');
 
